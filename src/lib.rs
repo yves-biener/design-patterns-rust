ab3d781a2d78e72bd8ee985063daa88a519e5253
Yves Biener <yves.biener@gmx.de>
2022-04-25 18:14:44 +0200

    mod: removed lib.rs default test add: creational patterns module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
 mod structural;
 mod behavioral;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod creational;
